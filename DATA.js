cfbf4dbac7b7175c275c9545b405d0618d54e7db
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 03:46:06 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "05fa7771-1b1b-4788-ac96-70fb07ef238d",
+    "home_feed": [
+        {
+            "id": "560c5b87-1706-4991-a5ed-80a777170e4d",
+            "artist_name": "left",
+            "home_carousel": [
+                {
+                    "id": "89915ec5-5dc1-48e4-857a-774fc3852908",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/19.png"
+                },
+                {
+                    "id": "8bd49b64-d70d-499e-9f73-b516cfb10df1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/78.png"
+                },
+                {
+                    "id": "3cddfa24-756a-41ba-aa4a-4e6e1eae4397",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/168.png"
+                },
+                {
+                    "id": "bbc104d0-4e39-4191-9c8d-e2472a8f86f3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                },
+                {
+                    "id": "27c4fb23-d440-47ad-9879-460e85b4957b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                }
+            ],
+            "art_caption": "news",
+            "art_description": "Year upon involve recent indeed reality. Play law knowledge role heavy red weight. Fine data our sell. Turn walk when time decision whom."
+        },
+        {
+            "id": "1a7756fe-f138-41fc-b6df-80ce7fe5a69c",
+            "artist_name": "down",
+            "home_carousel": [
+                {
+                    "id": "40a7fd77-ef19-46d9-90ff-6ca67b16f265",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png"
+                },
+                {
+                    "id": "1a8b567c-cd9f-4958-a25a-d1a14f909fb7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/37.png"
+                },
+                {
+                    "id": "bb7912b2-e63f-4c5c-96a7-8cad193a890b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png"
+                },
+                {
+                    "id": "77fa8a80-82f9-4db2-90f5-f3b59be144c4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                },
+                {
+                    "id": "77603ec5-5494-41ce-a153-8707a7e4b7ff",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                }
+            ],
+            "art_caption": "mission",
+            "art_description": "Certain win ask American cultural. Behind weight room rise record Congress meeting."
+        },
+        {
+            "id": "7b00b5fa-cc32-4a33-9600-4b8c89934f08",
+            "artist_name": "college",
+            "home_carousel": [
+                {
+                    "id": "3bb7aa90-d46d-43bd-b677-dd442f6fb18b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/135.png"
+                },
+                {
+                    "id": "3bd89b5a-98f6-4ca8-b82e-dd83bab1522e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/41.png"
+                },
+                {
+                    "id": "83a673e4-5fca-4cd2-9371-c47e46713854",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/270.png"
+                },
+                {
+                    "id": "91eb2380-f3cb-41b0-9c40-ffd59660f7d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/234.png"
+                },
+                {
+                    "id": "bdedfceb-7b35-4156-a3c2-0510e5fa4043",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/53.png"
+                }
+            ],
+            "art_caption": "character",
+            "art_description": "Every event unit store leave. Adult them analysis paper Mr."
+        },
+        {
+            "id": "7e33488c-1665-4f61-886e-8848665d5d50",
+            "artist_name": "experience",
+            "home_carousel": [
+                {
+                    "id": "99ddf428-f1d6-445a-a534-651a8bffa034",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/70.png"
+                },
+                {
+                    "id": "c5915e51-7d48-4022-b874-f0b2a90e3bc0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/357.png"
+                },
+                {
+                    "id": "25fb4538-c292-45c6-b8ad-bfa9c54925c3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                },
+                {
+                    "id": "5e760a05-a3b1-42cf-bb28-6e9c8ee0ceeb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png"
+                },
+                {
+                    "id": "1bb5bf1b-157c-410a-9db6-b7f1be9dd488",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png"
+                }
+            ],
+            "art_caption": "list",
+            "art_description": "Either film middle collection. Rate when exist back itself. Minute security argue crime PM."
+        },
+        {
+            "id": "159a3a1a-173b-4eca-9ad8-1de24405908a",
+            "artist_name": "imagine",
+            "home_carousel": [
+                {
+                    "id": "7a268f09-bca6-4876-84f3-b8bb64e9cd38",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png"
+                },
+                {
+                    "id": "bb526a70-d379-4185-a3c4-ca4ff71cb1eb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/185.png"
+                },
+                {
+                    "id": "35c5233a-0e76-4915-a9b7-c0cd5e46c8e3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+                },
+                {
+                    "id": "c8977ed3-8286-476e-a72a-3c68236f2669",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png"
+                },
+                {
+                    "id": "b5ac8aaa-99cd-4546-814d-d36325bbbc1f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/162.png"
+                }
+            ],
+            "art_caption": "option",
+            "art_description": "Assume whose my let Mr respond environment politics. Generation purpose on professional time six."
+        }
+    ]
+}
